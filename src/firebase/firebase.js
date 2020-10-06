1a80a35bbbbf701f1adeadad3091307998644194
Anjali <anjali.payal69@gmail.com>
2020-10-06 15:03:04 +0200

    Fetching Data From Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,30 +13,63 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Anjali', 
-    age: 19,
-    // isSingle: true,
-    stressLevel: 8,
-    job: {
-        title: 'Software Developer',
-        company: 'Google'
-    },
-    location: {
-        city: 'Woodmead',
-        country: 'South Africa'
-    }
-}).then(() => {
-    console.log('Data is saved');
-}).catch((e) => {
-    console.log('This failed', e);
+database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
 });
 
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
-});
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//     database.ref('age').set(20);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('age').set(21);
+// }, 10500);
+
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data', e);
+//     })
+
+// database.ref().set({
+//     name: 'Anjali', 
+//     age: 19,
+//     // isSingle: true,
+//     stressLevel: 8,
+//     job: {
+//         title: 'Software Developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Woodmead',
+//         country: 'South Africa'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {
+//     console.log('This failed', e);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
 
 // database.ref('isSingle').remove()
 //   .then(() => {
