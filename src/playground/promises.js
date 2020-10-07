b808066a4aef16129a59e13ed5450d66f3a2af66
Anjali <anjali.payal69@gmail.com>
2020-10-07 16:46:10 +0200

    Testing Async Redux Actions Part 1

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -3,12 +3,12 @@ const promise = new Promise((resolve, reject) => {
 
         // resolve('This is my resolved data');
         // resolve('This is my other resolved data'); Will be ignored when called 2nd
-        // resolve({
-        //     name: 'Anjali',
-        //     age: 19
-        // });
+        resolve({
+            name: 'Anjali',
+            age: 19
+        });
 
-        reject('Something went wrong');
+        // reject('Something went wrong');
 
     }, 5000);    
 });
@@ -17,6 +17,9 @@ console.log('before');
 
 promise.then((data) => {
     console.log('1', data);
+    return 'Some Data';
+}).then((str) => {
+    console.log('does this run?', str);
 }).catch((error) => {
     console.log('error: ' , error);
 });
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,9 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+
+const createMockStore = configureMockStore([thunk]);
 
 // TEST FOR REMOVE_EXPENSE
 test('should setup remove expense actions object', () => {
@@ -28,28 +33,42 @@ test('should setup add expense actions object with provided values', () => {
         createdAt: 1000,
         note: 'This was last months rent'
     };
-    const action = addExpense( expenseData );
+    const action = addExpense( expenses[2] );
     expect(action).toEqual({
         type: 'ADD_EXPENSE',
-        expense: {
-            ...expenseData,
-            //We cant type the id becausse the id keeps on changing
-            id: expect.any(String)
-        }
+        expense: expenses[2]
     });
 });
 
-// 2 - WHEN YOU USE THE DEFAULT VALUES
-test('should setup add expense actions object with default values', () => {
-    const action = addExpense(  );
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            id: expect.any(String),
-            description: '',
-            note: '',
-            amount: 0,
-            createdAt: 0
-        }        
+test('should add expense to database and store', (done) => {
+    const store = createMockStore({});
+    const expenseData = {
+        description: 'Mouse',
+        amount: 3000,
+        note: 'This one is better',
+        createdAt: 1000
+    };
+    store.dispatch(startAddExpense(expenseData)).then(() => {
+        expect(1).toBe(1);
+        done();
     });
 });
+
+test('should add expense with defaults to database and store', () => {
+
+});
+
+// 2 - WHEN YOU USE THE DEFAULT VALUES
+// test('should setup add expense actions object with default values', () => {
+//     const action = addExpense(  );
+//     expect(action).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             id: expect.any(String),
+//             description: '',
+//             note: '',
+//             amount: 0,
+//             createdAt: 0
+//         }        
+//     });
+// });
