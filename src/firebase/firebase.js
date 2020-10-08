d36ad552e085022988e8019372f5f9ddd54dc014
Anjali <anjali.payal69@gmail.com>
2020-10-08 19:27:35 +0200

    Login Page and Google Authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,8 +12,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // // subscribe to the child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
